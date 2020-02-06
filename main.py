a49a00dbf5bfe743d04454eb564ec04b7ef66751
myriamkapon@yahoo.gr <Mvakbo2798!!>
2020-02-06 02:41:07 +0200

    synchronize filtering to books and users

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pandas as pd
 
-df = pd.read_csv("data/BX-Book-Ratings.csv", sep=";", encoding = "ISO-8859-1")
+ratings = pd.read_csv("data/BX-Book-Ratings.csv", sep=";", encoding ="ISO-8859-1")
+print(ratings["ISBN"].value_counts())
 
-filterBooks = df[(df.groupby('User-ID')['Book-Rating'].transform('count') >= 5)]
-filterUsers = df[(df.groupby('ISBN')['Book-Rating'].transform('count') >= 10)]
 
-filtered3 = filterBooks.merge(filterUsers)
-print (filtered3)
+filterBooks = ratings[(ratings.groupby('ISBN')['Book-Rating'].transform('count') >= 10)]
+filterUsers = ratings[(ratings.groupby('User-ID')['Book-Rating'].transform('count') >= 5)]
+
+filteredRatings = filterBooks.merge(filterUsers)
+#print(filteredRatings)
+
+users = pd.read_csv("data/BX-Users.csv", sep=";", encoding ="ISO-8859-1")
+cond = ~users['User-ID'].isin(filteredRatings['User-ID']) == True
+users.drop(users[cond].index, inplace = True)
+
+books = pd.read_csv("data/BX-Books.csv", encoding ="ISO-8859-1", low_memory=False)
+print(books)
+cond = ~books['ISBN'].isin(filteredRatings['ISBN']) == True
+books.drop(books[cond].index, inplace = True)
+
+
+
